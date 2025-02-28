4e1dd0ff563b06b15783a0016043af71b18a1e81
Max Kucher <161813533+Cossomoj@users.noreply.github.com>
2025-02-28 09:06:25 +0300

    Update rag_service.py

--- src/main_version/rag_service.py
+++ src/main_version/rag_service.py
@@ -219,4 +219,4 @@ async def websocket_endpoint(websocket: WebSocket):
 if __name__ == "__main__":
     import uvicorn
     print("Запускаем сервер на ws://127.0.0.1:8000/ws")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
